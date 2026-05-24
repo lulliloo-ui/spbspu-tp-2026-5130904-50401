c2cb0dab9d9983a6afed689002c9bf1417c3ac2c
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-05-24 12:41:14 +0300

    cg

--- madieva.tatyana/T2/main.cpp
+++ madieva.tatyana/T2/main.cpp
@@ -1,7 +1,5 @@
 #include <iostream>
-#include <sstream>
 #include <string>
-#include <cassert>
 #include <iterator>
 #include <vector>
 #include <iomanip>
@@ -48,7 +46,6 @@ namespace madieva {
   std::istream & operator>>(std::istream & in, StringIO && dest);
   std::istream & operator>>(std::istream & in, LabelIO && dest);
   std::istream & operator>>(std::istream & in, DataStruct & dest);
-
   std::ostream & operator<<(std::ostream & out, const DataStruct & src);
 
   class IOguard
@@ -64,178 +61,171 @@ namespace madieva {
     char fill_;
   };
 
-  bool DataStruct::operator<(const DataStruct& rhs) const
-  {
-    if (key1 != rhs.key1) {
-      return key1 < rhs.key1;
-    }
-    if (key2 != rhs.key2) {
-      return key2 < rhs.key2;
-    }
-    return key3.length() < rhs.key3.length();
+}
+bool madieva::DataStruct::operator<(const DataStruct& rhs) const
+{
+  if (key1 != rhs.key1) {
+    return key1 < rhs.key1;
   }
+  if (key2 != rhs.key2) {
+    return key2 < rhs.key2;
+  }
+  return key3.length() < rhs.key3.length();
+}
 
-  std::istream & operator>>(std::istream & in, DelimiterIO && dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    char c = ' ';
-    in >> c;
-    if (in && (c != dest.expc)) {
-      in.setstate(std::ios::failbit);
-    }
+std::istream & madieva::operator>>(std::istream & in, DelimiterIO && dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  char c = ' ';
+  in >> c;
+  if (in && (c != dest.expc)) {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & madieva::operator>>(std::istream & in, DoubleIO && dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  in >> dest.ref;
+  if (!in) {
+    return in;
+  }
+  char suffix = ' ';
+  in >> suffix;
+  if (suffix != 'd' && suffix != 'D') {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & madieva::operator>>(std::istream & in, ULLIO && dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  in >> dest.ref;
+  if (!in) {
+    return in;
+  }
+  char c1 = ' ', c2 = ' ', c3 = ' ';
+  in >> c1 >> c2 >> c3;
+  bool valid = ((c1 == 'u' || c1 == 'U') &&
+    (c2 == 'l' || c2 == 'L') &&
+    (c3 == 'l' || c3 == 'L'));
+  if (!valid) {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & madieva::operator>>(std::istream & in, StringIO && dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
+}
+
+std::istream & madieva::operator>>(std::istream & in, LabelIO && dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
+    return in;
+  }
+  std::string word = "";
+  in >> word;
+  if (in && (word != dest.exp)) {
+    in.setstate(std::ios::failbit);
+  }
+  return in;
+}
+
+std::istream & madieva::operator>>(std::istream & in, DataStruct & dest)
+{
+  std::istream::sentry sentry(in);
+  if (!sentry) {
     return in;
   }
 
-  std::istream & operator>>(std::istream & in, DoubleIO && dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    in >> dest.ref;
-    if (!in) {
-      return in;
-    }
-    char suffix = ' ';
-    in >> suffix;
-    if (suffix != 'd' && suffix != 'D') {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
+  double k1 = 0;
+  unsigned long long k2 = 0.0;
+  std::string k3 = "";
+  std::string key;
+  bool have1 = false, have2 = false, have3 = false;
 
-  std::istream & operator>>(std::istream & in, ULLIO && dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    in >> dest.ref;
-    if (!in) {
-      return in;
-    }
-    char c1 = ' ', c2 = ' ', c3 = ' ';
-    in >> c1 >> c2 >> c3;
-
-    bool valid = ((c1 == 'u' || c1 == 'U') &&
-      (c2 == 'l' || c2 == 'L') &&
-      (c3 == 'l' || c3 == 'L'));
-
-    if (!valid) {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
-
-  std::istream & operator>>(std::istream & in, StringIO && dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    return std::getline(in >> DelimiterIO{'"'}, dest.ref, '"');
-  }
-
-  std::istream & operator>>(std::istream & in, LabelIO && dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-    std::string word = "";
-    in >> word;
-    if (in && (word != dest.exp)) {
-      in.setstate(std::ios::failbit);
-    }
-    return in;
-  }
-
-  std::istream & operator>>(std::istream & in, DataStruct & dest)
-  {
-    std::istream::sentry sentry(in);
-    if (!sentry) {
-      return in;
-    }
-
-    double k1 = 0;
-    unsigned long long k2 = 0;
-    std::string k3 = "";
-    std::string key;
-
-    bool have1 = false, have2 = false, have3 = false;
-
-    in >> DelimiterIO{'('};
-    in >> DelimiterIO{':'};
-    for (int i = 0; i < 3 && in; ++i) {
-      in >> key;
-      if (key == "key1") {
-        in >> DoubleIO{k1};
-        if (in) have1 = true;
-      } else if (key == "key2") {
-        in >> ULLIO{k2};
-        if (in) have2 = true;
-      } else if (key == "key3") {
-        in >> StringIO{k3};
-        if (in) have3 = true;
-      } else {
-        in.setstate(std::ios::failbit);
-        return in;
-      }
-      in >> DelimiterIO{':'};
-    }
-    in >> DelimiterIO{')'};
-    if (have1 && have2 && have3) {
-      dest.key1 = k1;
-      dest.key2 = k2;
-      dest.key3 = k3;
+  in >> DelimiterIO{'('};
+  in >> DelimiterIO{':'};
+  for (int i = 0; i < 3 && in; ++i) {
+    in >> key;
+    if (key == "key1") {
+      in >> DoubleIO{k1};
+      if (in) have1 = true;
+    } else if (key == "key2") {
+      in >> ULLIO{k2};
+      if (in) have2 = true;
+    } else if (key == "key3") {
+      in >> StringIO{k3};
+      if (in) have3 = true;
     } else {
       in.setstate(std::ios::failbit);
+      return in;
     }
-    if (!in) {
-      in.clear();
-      in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
-      return in >> dest;
-    }
-    return in;
+    in >> DelimiterIO{':'};
   }
+  in >> DelimiterIO{')'};
+  if (have1 && have2 && have3) {
+    dest.key1 = k1;
+    dest.key2 = k2;
+    dest.key3 = k3;
+  } else {
+    in.setstate(std::ios::failbit);
+  }
+  if (!in) {
+    in.clear();
+    in.ignore(std::numeric_limits<std::streamsize>::max(), '\n');
+    return in >> dest;
+  }
+  return in;
+}
 
-  std::ostream & operator<<(std::ostream  & out, const DataStruct & src)
-  {
-    std::ostream::sentry sentry(out);
-    if (!sentry) {
-      return out;
-    }
-
-    IOguard guard(out);
-    out << "(:key1 " << std::fixed << std::setprecision(1) << src.key1 << "d";
-    out << ":key2 " << src.key2 << "ull";
-    out << ":key3 \"" << src.key3 << "\":)";
+std::ostream & madieva::operator<<(std::ostream  & out, const DataStruct & src)
+{
+  std::ostream::sentry sentry(out);
+  if (!sentry) {
     return out;
   }
 
-  IOguard::IOguard(std::basic_ios< char > & s) :
-    s_(s),
-    width_(s.width()),
-    precision_(s.precision()),
-    fmt_(s.flags()),
-    fill_(s.fill())
-  {}
-
-  IOguard::~IOguard()
-  {
-    s_.width(width_);
-    s_.precision(precision_);
-    s_.flags(fmt_);
-    s_.fill(fill_);
-  }
-
+  IOguard guard(out);
+  out << "(:key1 " << std::fixed << std::setprecision(1) << src.key1 << "d";
+  out << ":key2 " << src.key2 << "ull";
+  out << ":key3 \"" << src.key3 << "\":)";
+  return out;
 }
 
+madieva::IOguard::IOguard(std::basic_ios< char > & s) :
+  s_(s),
+  width_(s.width()),
+  precision_(s.precision()),
+  fmt_(s.flags()),
+  fill_(s.fill())
+{}
 
-
+madieva::IOguard::~IOguard()
+{
+  s_.width(width_);
+  s_.precision(precision_);
+  s_.flags(fmt_);
+  s_.fill(fill_);
+}
 
 int main()
 {
@@ -243,15 +233,8 @@ int main()
   using T = DataStruct;
   std::vector< T > data;
   using iit_t = std::istream_iterator< T >;
-
-  // std::string test = "(:key2 42ull:key1 3.14d:key3 \"hello\":)";
-  // std::istringstream iss(test);
-  // DataStruct ds;
-
   std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
-
   std::sort(data.begin(), data.end());
-
   using oit_t = std::ostream_iterator< DataStruct >;
   std::copy(std::begin(data), std::end(data), oit_t{std::cout, "\n"});
 
