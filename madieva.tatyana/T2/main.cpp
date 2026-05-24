e4aa309059a5447c93e6c7b5cfca4adbd88ab7a2
lulliloo-ui <madieva.tv@edu.spbstu.ru>
2026-05-24 12:25:15 +0300

    sort

--- madieva.tatyana/T2/main.cpp
+++ madieva.tatyana/T2/main.cpp
@@ -6,6 +6,7 @@
 #include <vector>
 #include <iomanip>
 #include <limits>
+#include <algorithm>
 
 namespace madieva {
   struct DataStruct
@@ -248,9 +249,10 @@ int main()
   // DataStruct ds;
 
   std::copy(iit_t{std::cin}, iit_t{}, std::back_inserter(data));
+
+  std::sort(data.begin(), data.end());
+
   using oit_t = std::ostream_iterator< DataStruct >;
   std::copy(std::begin(data), std::end(data), oit_t{std::cout, "\n"});
-
-
 
 }
